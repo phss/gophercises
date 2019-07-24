9de5612cf311d6447f8e11b7a7ee4e81cfbe711a
Paulo Schneider <paulo.schneider@gmail.com>
2019-07-24 14:32:47 +0100

    Print all questions from short.csv

--- quiz/main.go
+++ quiz/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/phss/gophercises/quiz/questions"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	defaultProblemsPath := "sample/short.csv"
+	problemsFile, _ := os.Open(defaultProblemsPath)
+
+	problems, _ := questions.Load(problemsFile)
+
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s =\n", i+1, problem.Question)
+	}
+	fmt.Printf("You scored 0 out of %d.\n", len(problems))
 }
--- quiz/main_test.go
+++ quiz/main_test.go
@@ -3,10 +3,16 @@ package main_test
 import (
 	"os/exec"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestMain(t *testing.T) {
-	expected := "Hello world\n"
+	expected := `Problem #1: 1+1 =
+Problem #2: 2+3 =
+Problem #3: 13+17 =
+You scored 0 out of 3.
+`
 
 	cmd := exec.Command("go", "run", ".")
 	output, err := cmd.CombinedOutput()
@@ -15,7 +21,7 @@ func TestMain(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if actual != expected {
-		t.Fatalf("expected %s, got %s", expected, actual)
+	if diff := cmp.Diff(expected, actual); diff != "" {
+		t.Fatal(diff)
 	}
 }
