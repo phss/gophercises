0ef9e6e36c2befd88d53e8b85c27d0b56409b8c0
Paulo Schneider <paulo.schneider@gmail.com>
2019-07-24 15:03:55 +0100

    Move to a test table format

--- quiz/main_test.go
+++ quiz/main_test.go
@@ -3,20 +3,34 @@ package main_test
 import (
 	"io/ioutil"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestMain(t *testing.T) {
-	cmd := exec.Command("go", "run", ".")
-	actual, err := cmd.CombinedOutput()
-	expected, _ := ioutil.ReadFile("testdata/TestMain.golden")
-
-	if err != nil {
-		t.Fatal(err)
+	testTable := []struct {
+		name string
+	}{
+		{name: "default params"},
 	}
-	if diff := cmp.Diff(string(expected), string(actual)); diff != "" {
-		t.Fatal(diff)
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			expected, err := ioutil.ReadFile(filepath.Join("testdata", t.Name()+".golden"))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			cmd := exec.Command("go", "run", ".")
+			actual, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(string(expected), string(actual)); diff != "" {
+				t.Fatal(diff)
+			}
+		})
 	}
 }
