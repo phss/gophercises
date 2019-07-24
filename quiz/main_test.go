9163d6b9d97f32eeb61a04c8c699d1c392c6b8eb
Paulo Schneider <paulo.schneider@gmail.com>
2019-07-24 14:55:04 +0100

    Move expected output to a golden file

--- quiz/main_test.go
+++ quiz/main_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"io/ioutil"
 	"os/exec"
 	"testing"
 
@@ -8,20 +9,14 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	expected := `Problem #1: 1+1 =
-Problem #2: 2+3 =
-Problem #3: 13+17 =
-You scored 0 out of 3.
-`
-
 	cmd := exec.Command("go", "run", ".")
-	output, err := cmd.CombinedOutput()
-	actual := string(output)
+	actual, err := cmd.CombinedOutput()
+	expected, _ := ioutil.ReadFile("testdata/TestMain.golden")
 
 	if err != nil {
 		t.Fatal(err)
 	}
-	if diff := cmp.Diff(expected, actual); diff != "" {
+	if diff := cmp.Diff(string(expected), string(actual)); diff != "" {
 		t.Fatal(diff)
 	}
 }
