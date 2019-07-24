d568fd2bb1a6905b9bae4f3dac8a5f64777eee69
Paulo Schneider <paulo.schneider@gmail.com>
2019-07-24 10:25:59 +0100

    Refactoring quiz question tests

--- quiz/questions/questions_test.go
+++ quiz/questions/questions_test.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+
 	"github.com/phss/gophercises/quiz/questions"
 )
 
@@ -20,16 +22,10 @@ is this a valid question?,yes
 one more?,sure`))
 
 	if err != nil {
-		t.Errorf("Didn't expect error %v", err)
+		t.Errorf("got error %v", err)
 	}
-	if len(actualQuestions) != len(expectedQuestions) {
-		t.Errorf("Mismatch number of questions. Expected %v but got %v.", len(expectedQuestions), len(actualQuestions))
-	}
-	for i := range actualQuestions {
-		if actualQuestions[i] != expectedQuestions[i] {
-			t.Errorf("On index %v expected %v but got %v", i, expectedQuestions[i], actualQuestions[i])
-		}
-
+	if diff := cmp.Diff(actualQuestions, expectedQuestions); diff != "" {
+		t.Error(diff)
 	}
 }
 
