d7e7814756d8109874d3e3877025790cc0ccf7cc
Paulo Schneider <paulo.schneider@gmail.com>
2019-07-25 11:06:58 +0100

    Convert game test to a table test

--- quiz/game/game_test.go
+++ quiz/game/game_test.go
@@ -12,21 +12,35 @@ import (
 )
 
 func TestPlay(t *testing.T) {
-	expected, err := ioutil.ReadFile(filepath.Join("..", "testdata", t.Name()+".golden"))
-	if err != nil {
-		t.Fatal(err)
+	testTable := []struct {
+		name     string
+		problems []problems.Problem
+		answers  []string
+	}{
+		{
+			name: "normal game",
+			problems: []problems.Problem{
+				{Question: "1+1", Answer: "2"},
+				{Question: "is this a valid question?", Answer: "yes"},
+				{Question: "one more?", Answer: "sure"},
+			},
+			answers: []string{"2", "yes", "nope"},
+		},
 	}
-	problems := []problems.Problem{
-		{Question: "1+1", Answer: "2"},
-		{Question: "is this a valid question?", Answer: "yes"},
-		{Question: "one more?", Answer: "sure"},
-	}
-	answers := strings.NewReader("2\nyes\nnope")
-	output := strings.Builder{}
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			expected, err := ioutil.ReadFile(filepath.Join("..", "testdata", t.Name()+".golden"))
+			if err != nil {
+				t.Fatal(err)
+			}
+			answers := strings.NewReader(strings.Join(testCase.answers, "\n"))
+			output := strings.Builder{}
 
-	game.Play(&problems, answers, &output)
+			game.Play(&testCase.problems, answers, &output)
 
-	if diff := cmp.Diff(string(expected), output.String()); diff != "" {
-		t.Fatal(diff)
+			if diff := cmp.Diff(string(expected), output.String()); diff != "" {
+				t.Fatal(diff)
+			}
+		})
 	}
 }
