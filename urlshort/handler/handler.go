78fc85882ef120c6facc7efcd3b63c1bf26d2d6c
Paulo Schneider <paulo.schneider@gmail.com>
2019-07-25 17:21:32 +0100

    Redirect to urls from yaml config

--- urlshort/handler/handler.go
+++ urlshort/handler/handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler redirects requests for registered paths, in map format, otherwise fallbacks.
@@ -18,7 +20,25 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 
 // YAMLHandler redirects requests for registered paths, in YAML format, otherwise fallbacks.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fallback.ServeHTTP(w, r)
-	}, nil
+	pathsToUrls, err := yamlToMap(yml)
+	return MapHandler(pathsToUrls, fallback), err
+}
+
+func yamlToMap(yml []byte) (map[string]string, error) {
+	type YamlData struct {
+		Path string `yaml:path`
+		Url  string `yaml:url`
+	}
+
+	yamlData := []YamlData{}
+	err := yaml.Unmarshal(yml, &yamlData)
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := make(map[string]string)
+	for _, data := range yamlData {
+		pathsToUrls[data.Path] = data.Url
+	}
+	return pathsToUrls, nil
 }
--- urlshort/handler/handler_test.go
+++ urlshort/handler/handler_test.go
@@ -3,19 +3,15 @@ package handler_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/phss/gophercises/urlshort/handler"
 )
 
-var fallback http.HandlerFunc
-
 func TestMapHandler(t *testing.T) {
 	pathsToUrls := map[string]string{
 		"/in-map": "http://example.com/in-map",
-	}
-	fallback = func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "http://example.com/redirect-through-fallback", http.StatusSeeOther)
 	}
 
 	tt := []struct {
@@ -31,7 +27,7 @@ func TestMapHandler(t *testing.T) {
 			request, _ := http.NewRequest("GET", tc.requestPath, nil)
 			response := httptest.NewRecorder()
 
-			handler := handler.MapHandler(pathsToUrls, fallback)
+			handler := handler.MapHandler(pathsToUrls, fallbackHandler())
 			handler.ServeHTTP(response, request)
 
 			actualRedirect := response.Header().Get("Location")
@@ -39,5 +35,49 @@ func TestMapHandler(t *testing.T) {
 				t.Fatalf("redirect expected %s, got %s", tc.expectedRedirect, actualRedirect)
 			}
 		})
+	}
+}
+
+func TestYAMLHandler(t *testing.T) {
+	yaml := `
+- path: /in-yaml
+  url: http://example.com/in-yaml
+`
+
+	tt := []struct {
+		name             string
+		requestPath      string
+		expectedRedirect string
+	}{
+		{name: "path in map", requestPath: "/in-yaml", expectedRedirect: "http://example.com/in-yaml"},
+		{name: "fallback", requestPath: "/not-in-yaml", expectedRedirect: "http://example.com/redirect-through-fallback"},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			request, _ := http.NewRequest("GET", tc.requestPath, nil)
+			response := httptest.NewRecorder()
+
+			handler, _ := handler.YAMLHandler([]byte(yaml), fallbackHandler())
+			handler.ServeHTTP(response, request)
+
+			actualRedirect := response.Header().Get("Location")
+			if actualRedirect != tc.expectedRedirect {
+				t.Fatalf("redirect expected %s, got %s", tc.expectedRedirect, actualRedirect)
+			}
+		})
+	}
+}
+
+func TestYAMLHandler_invalidYAML(t *testing.T) {
+	_, err := handler.YAMLHandler([]byte("bad yaml"), fallbackHandler())
+
+	if !strings.HasPrefix(err.Error(), "yaml: unmarshal errors") {
+		t.Fatalf("error %v", err)
+	}
+}
+
+func fallbackHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "http://example.com/redirect-through-fallback", http.StatusSeeOther)
 	}
 }
